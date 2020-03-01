7803360300269c9da7a31868d31b7f4823ca411e
Loc Hinh <lhinh3431@gmail.com>
2020-02-29 22:21:28 -0800

    added Employees, FunctionalAreas, Programs to admin page

--- bug_hunt/app/admin.py
+++ bug_hunt/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Employees, FunctionalAreas, Programs
 # Register your models here.
+
+admin.site.register(Employees)
+admin.site.register(FunctionalAreas)
+admin.site.register(Programs)
