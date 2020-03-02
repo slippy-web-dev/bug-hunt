2eaba8a437bfa79108145d67e993e52f501f3cef
Loc Hinh <lhinh3431@gmail.com>
2020-03-01 17:02:26 -0800

    added login and logout routes. login redirects to /app/index/ and logout redirects to /app/login/

--- bug_hunt/app/urls.py
+++ bug_hunt/app/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as django_auth_views
 
 app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('new-bug', views.new_bug, name='new_bug'),
-    path('update-bug', views.update_bug, name='update_bug'),
-    path('admin/db-maintenance', views.database_maintenance, name='database_maintenance'),
-    path('admin/edit-add-areas', views.edit_add_areas, name='edit_add_areas'),
-    path('admin/add-programs', views.add_programs, name='add_programs'),
-    path('admin/edit-programs', views.edit_programs, name='edit_programs'),
-    path('admin/add-employees', views.add_employees, name='add_employees'),
-    path('admin/edit-employees', views.edit_employees, name='edit_employees'),
-    path('admin/export-areas', views.export_areas, name='export_areas'),
+    path('login/', django_auth_views.LoginView.as_view(), name='login'),
+    path('logout/', django_auth_views.LogoutView.as_view(), name='logout'),
+    path('index/', views.index, name='index'),
+    path('new-bug/', views.new_bug, name='new_bug'),
+    path('update-bug/', views.update_bug, name='update_bug'),
+    path('admin/db-maintenance/', views.database_maintenance, name='database_maintenance'),
+    path('admin/edit-add-areas/', views.edit_add_areas, name='edit_add_areas'),
+    path('admin/add-programs/', views.add_programs, name='add_programs'),
+    path('admin/edit-programs/', views.edit_programs, name='edit_programs'),
+    path('admin/add-employees/', views.add_employees, name='add_employees'),
+    path('admin/edit-employees/', views.edit_employees, name='edit_employees'),
+    path('admin/export-areas/', views.export_areas, name='export_areas'),
 ]
