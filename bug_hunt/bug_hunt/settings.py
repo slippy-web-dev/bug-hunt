9e9b4b3c18000530b5d3a4009aac87b3299de2d3
Phuc Dang <dragon31194@gmail.com>
2020-04-20 15:19:13 -0700

    frontend update for the rest

--- bug_hunt/bug_hunt/settings.py
+++ bug_hunt/bug_hunt/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/templates/static_files'),
     os.path.join(BASE_DIR, 'bootstrap'),
