417b60bd0ec443800a8c477d99ca85d65b6a0152
Loc Hinh <lhinh3431@gmail.com>
2020-04-27 18:26:34 -0700

    registered every table into django "admin" page

--- bug_hunt/app/admin.py
+++ bug_hunt/app/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import AccessLevels, Severities, AttachmentTypes, ReportTypes, Status, Priorities, Resolutions
+import models as m
+# from .models import AccessLevels, Severities, AttachmentTypes, ReportTypes, Status, Priorities, Resolutions
 # Register your models here.
 
-admin.site.register(AccessLevels)
-admin.site.register(Severities)
-admin.site.register(AttachmentTypes)
-admin.site.register(ReportTypes)
-admin.site.register(Status)
-admin.site.register(Priorities)
-admin.site.register(Resolutions)
+admin.site.register(m.Programs)
+admin.site.register(m.FunctionalAreas)
+admin.site.register(m.AccessLevels)
+admin.site.register(m.Employees)
+admin.site.register(m.ReportTypes)
+admin.site.register(m.Severities)
+admin.site.register(m.Status)
+admin.site.register(m.Priorities)
+admin.site.register(m.Resolutions)
+admin.site.register(m.AttachmentTypes)
+admin.site.register(m.BugReports)
+admin.site.register(m.Attachments)
